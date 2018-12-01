ace3fc53ac9783a241b8210977ff13c53a8d2fa2
Zebat2 <zebat2@illinois.edu>
2018-12-01 15:51:29 -0600

    Added a button

--- app/src/main/java/com/example/zebatabassum/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/zebatabassum/cs125finalproject/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.zebatabassum.cs125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
+
