9629623377251505373aa91b0436281058f83a48
Zebat2 <zebat2@illinois.edu>
2018-12-06 17:50:00 -0600

    parsed

--- app/src/main/java/com/example/zebatabassum/cs125finalproject/CompSci.java
+++ app/src/main/java/com/example/zebatabassum/cs125finalproject/CompSci.java
@@ -2,12 +2,31 @@ package com.example.zebatabassum.cs125finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CompSci extends AppCompatActivity {
-
+    Button getQuestionCS;
+    public static TextView data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comp_sci);
+
+        getQuestionCS = (Button) findViewById(R.id.getQuestionCS);
+        data = (TextView) findViewById(R.id.questions);
+
+
+        getQuestionCS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getQuestion process = new getQuestion();
+                process.execute();
+            }
+        });
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/zebatabassum/cs125finalproject/getQuestion.java
@@ -0,0 +1,72 @@
+package com.example.zebatabassum.cs125finalproject;
+
+import android.os.AsyncTask;
+
+import org.json.*;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class getQuestion extends AsyncTask<Void,Void,Void>{
+    String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://opentdb.com/api.php?amount=10&category=18&type=boolean");
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONObject arr = new JSONObject(data);
+            JSONArray results = (JSONArray) (arr.get("results"));
+            String qq ="";
+            JSONArray jsonarray = results;
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                 qq= jsonobject.getString("question");
+                //String url = jsonobject.getString("url");
+            }
+            //singleParsed =  results.get(4);
+            //JSONObject tst = new JSONObject(results);
+            Log.d("TAG", qq);
+            dataParsed = qq;//dataParsed + singleParsed + "\n";
+//            for(int i = 0; i <arr.length(); i++) {
+//                JSONObject obj = (JSONObject) arr.get(i);
+//                singleParsed = "Category: " + obj.get("results").get("question");
+//
+//
+//                dataParsed = dataParsed + singleParsed + "\n";
+//
+//            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        CompSci.data.setText(this.dataParsed);
+    }
+}
